f2d0627b7da451f51255556234485e397a43b041
Ahmed Elgendy <ahmed.elgendy@leanscale.com>
2020-04-30 18:18:30 +0300

    first commit

---
+++ Common/Common/Labels.swift
@@ -0,0 +1,13 @@
+//
+//  Labels.swift
+//  Common
+//
+//  Created by Elgendy on 29.04.2020.
+//  Copyright © 2020 Elgendy. All rights reserved.
+//
+
+import Foundation
+
+public class Labels {
+    public static let welcome = "WELCOME"
+}
